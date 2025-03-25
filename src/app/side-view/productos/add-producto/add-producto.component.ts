a7eb6554226313244601b56d591f0bc8f8f3a085
Aldordzvn <aldordzvn2001@gmail.com>
2025-03-25 11:13:46 -0700

    Logica de productos

--- src/app/side-view/productos/add-producto/add-producto.component.ts
+++ src/app/side-view/productos/add-producto/add-producto.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Producto } from '../producto.model';
+import { ProductosService } from '../../../productos.service';
 
 @Component({
   selector: 'app-add-producto',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './add-producto.component.html',
   styleUrl: './add-producto.component.scss'
 })
 export class AddProductoComponent {
-  
+  warningImg = 'img/warning.svg';
+  closeImg = 'img/close.svg';
+  nombre: string = '';
+  marca: string = '';
+  descripcion: string = '';
+  index!: number;
+  alerta: boolean = false;
+
+  constructor(private productoService: ProductosService, private route: Router) { }
+
+  // addProducto(event: Event){
+  //   if(this.nombre && this.marca && this.descripcion){
+  //     const producto: Producto = new Producto(this.nombre, this.marca, this.descripcion);
+  //     this.productoService.addProducto(producto);
+  //     this.route.navigate(['productos']);
+  //   }else{
+  //     this.alerta = true;
+  //   }
+  // }
+
+  toggleAlerta(){
+    this.alerta = !this.alerta;
+  }
+
+  addProducto(form: NgForm) {
+    if (!form.valid) {
+      this.alerta = true;
+    } else {
+      const producto: Producto = new Producto(this.index, this.nombre, this.marca, this.descripcion);
+      this.productoService.addProducto(producto);
+      this.route.navigate(['productos']);
+    }
+  }
+
+
 }
--- src/app/side-view/productos/productos.component.ts
+++ src/app/side-view/productos/productos.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ProductosService } from '../../productos.service';
+import { Producto } from './producto.model';
 
 @Component({
   selector: 'app-productos',
@@ -8,7 +10,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './productos.component.scss'
 })
 export class ProductosComponent {
+  productoVista: Producto[] = [];
   itemImg = 'img/item.svg';
   deleteImg = 'img/delete.svg';
   iconImg = 'img/add.svg';
+
+  constructor(private productoService:ProductosService, private route: ActivatedRoute){}
+
+  ngOnInit(){
+    this.productoVista = this.productoService.productosLista;
+  }
+
+  
 }
