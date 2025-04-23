b48d12791fea25943ed0f5375355a8cf45e05808
Aldordzvn <aldordzvn2001@gmail.com>
2025-04-23 13:21:14 -0700

    Update firebaseapp.service.ts
    Se eliminan credenciales de firebase

--- src/app/firebaseapp.service.ts
+++ src/app/firebaseapp.service.ts
@@ -9,13 +9,13 @@ import { initializeApp } from 'firebase/app';
 export class FirebaseappService {
 
   firebaseConfig = {
-    apiKey: "AIzaSyDJAxQcnQ9RAWmj1OEbAOYdPL2QG1hSekg",
-    authDomain: "crud-angular-532cd.firebaseapp.com",
-    databaseURL: "https://crud-angular-532cd-default-rtdb.firebaseio.com",
-    projectId: "crud-angular-532cd",
-    storageBucket: "crud-angular-532cd.firebasestorage.app",
-    messagingSenderId: "55148169998",
-    appId: "1:55148169998:web:752880a035ea1b55fb655f"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   public auth: Auth;
